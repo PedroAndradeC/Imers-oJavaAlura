ceb3dd2639695e59f5dc8f066e3b741c3a07ba48
pedroandradecosta@outlook.com <pedroandradecosta@outlook.com>
2023-04-04 16:36:45 -0300

    Adicionando um Enum com as configs

--- src/App.java
+++ src/App.java
@@ -5,12 +5,11 @@ import java.util.List;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // fazer uma conexão HTTP e buscar os top 250 filmes
-        // String url = "https://imdb-api.com/en/API/Top250Movies/k_5e95y37c";
-        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
-
-        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+        API api = API.NASA;
+        
+        String url = api.getUrl();
+        
+        ExtratorDeConteudo extrator = api.getExtrator();
 
         var http = new ClienteHttp();
         String json = http.buscaDados(url);
