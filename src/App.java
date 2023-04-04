5b03cd99d95d1ac4df48e29424e161ac6e203507
pedroandradecosta@outlook.com <pedroandradecosta@outlook.com>
2023-04-04 15:31:48 -0300

    Criando própria Exception

---
+++ src/App.java
@@ -0,0 +1,37 @@
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        // String url = "https://imdb-api.com/en/API/Top250Movies/k_5e95y37c";
+        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+
+        // exibir e manipular os dados 
+
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+        
+        var geradora = new GeradoraDeFigurinhas();
+
+        for (int i = 0; i < 3; i++) {
+
+            Conteudo conteudo = conteudos.get(i);
+
+            InputStream inputStream = new URL(conteudo.urlImagem()).openStream();
+            String nomeArquivo = "saída/" + conteudo.titulo() + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(conteudo.titulo());
+            System.out.println();
+        }
+    }
+}
---
+++ src/GeradoraDeFigurinhas.java
@@ -0,0 +1,44 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class GeradoraDeFigurinhas {
+    
+
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
+
+        // leitura de imagem
+        // InputStream inputStream = 
+        //          new FileInputStream(new File("entrada/filme.jpg"));
+        // InputStream inputStream = 
+        //            new URL("https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_Ratio0.6716_AL_.jpg").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // cria nova imagem em memória com transparência e com tamanho novo
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        // copiar a imagem original para nova imagem (em memória)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+
+        // configurar a fonte
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 100);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
+
+        // escrever uma frase na nova imagem
+        graphics.drawString("TOPZERA", 380, novaAltura - 80);
+
+        // escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
+
+    }
+}
